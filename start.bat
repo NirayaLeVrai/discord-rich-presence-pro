ab6502d4560595e5f9c93112b585b08c13b525ab
NirayaLeVrai <sdvspprt@gmail.com>
2025-05-18 22:45:16 +0200

    Start.bat
    le fichier du lancement !

---
+++ start.bat
@@ -0,0 +1,13 @@
+@echo off
+title Discord Rich Presence - Pro by Dynamia
+color 0A
+echo ============================================
+echo       Lancement de la Rich Presence...
+echo ============================================
+echo.
+npm start
+echo.
+echo ============================================
+echo       Appuyez sur une touche pour quitter...
+echo ============================================
+pause >nul
